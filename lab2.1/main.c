dee9926d2214000b1d0942310005195fa84b4ca8
midanroxy <47028567+midanroxy@users.noreply.github.com>
2019-05-23 20:33:11 +0300

    Add files via upload

---
+++ lab2.1/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int N;
+    double x, leftX = 0, rightX = 6.28;
+
+    printf("Enter x, N: ");
+    if (scanf("%lf %d", &x, &N) != 2) {
+        printf("Invalid input\n");
+        return -1;
+    }
+
+    if (x < leftX || x > rightX) {
+        char* fmt = "Incorrect %.4f. Need from [%.4f, %.4f]\n";
+        printf(fmt, x, leftX, rightX);
+        return -1;
+    }
+
+    double a = x, S = a;
+
+    for (int n = 0; n <= N; ++n) {
+        double q = - x/(2*n+1)*(2*n+2);
+        a *= q;
+        S += a;
+    }
+
+    double y = cos(sqrt(x));
+    printf("S=%.7f\n", S);
+    printf("y=%.7f\n", y);
+    double eps = fabs(y-S)/fabs(S)*100;
+    printf("e=%.2f%%\n", eps);
+
+    return 0;
+}
